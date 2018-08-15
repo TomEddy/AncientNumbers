daa88461f82d6d2f92e614e65b00f1cd8bee7952
Tom Eddy <tomeddy362@gmail.com>
2018-08-14 21:30:12 -0700

    Day 2
    Working on Mayan number system. Works for integers from 1-160000. The
    visuals are not perfect and simbol for zero is yet to be incorperated.

--- app/src/main/java/ancientnumbers/eddytom/com/ancientnumbers/MainActivity.java
+++ app/src/main/java/ancientnumbers/eddytom/com/ancientnumbers/MainActivity.java
@@ -1,6 +1,14 @@
 package ancientnumbers.eddytom.com.ancientnumbers;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.solver.widgets.Rectangle;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -17,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     TableLayout output;
     Button convert;
-    private ImageView r11, r12, r13, r14, r16, r17, r18, r19, r111, r112, r113, r114, r116, r117, r118, r119, r21, r22, r23, r24, r26, r27, r28, r29, r211, r212, r213, r214, r216, r217, r218, r219;
+    private ImageView r11, r12, r13, r14, r16, r17, r18, r19, r111, r112, r113, r114, r116, r117, r118, r119, r21, r22, r23, r24, r26, r27, r28, r29, r211, r212, r213, r214, r216, r217, r218, r219,r31, r32, r33, r34, r36, r37, r38, r39, r311, r312, r313, r314, r316, r317, r318, r319,r41, r42, r43, r44, r46, r47, r48, r49, r411, r412, r413, r414, r416, r417, r418, r419;
     ArrayList<ImageView> nodeList;
     int counter = 0;
 
@@ -64,6 +72,38 @@ public class MainActivity extends AppCompatActivity {
         r217 = findViewById(R.id.r217);
         r218 = findViewById(R.id.r218);
         r219 = findViewById(R.id.r219);
+        r31 = findViewById(R.id.r31);
+        r32 = findViewById(R.id.r32);
+        r33 = findViewById(R.id.r33);
+        r34 = findViewById(R.id.r34);
+        r36 = findViewById(R.id.r36);
+        r37 = findViewById(R.id.r37);
+        r38 = findViewById(R.id.r38);
+        r39 = findViewById(R.id.r39);
+        r311 = findViewById(R.id.r311);
+        r312 = findViewById(R.id.r312);
+        r313 = findViewById(R.id.r313);
+        r314 = findViewById(R.id.r314);
+        r316 = findViewById(R.id.r316);
+        r317 = findViewById(R.id.r317);
+        r318 = findViewById(R.id.r318);
+        r319 = findViewById(R.id.r319);
+        r41 = findViewById(R.id.r41);
+        r42 = findViewById(R.id.r42);
+        r43 = findViewById(R.id.r43);
+        r44 = findViewById(R.id.r44);
+        r46 = findViewById(R.id.r46);
+        r47 = findViewById(R.id.r47);
+        r48 = findViewById(R.id.r48);
+        r49 = findViewById(R.id.r49);
+        r411 = findViewById(R.id.r411);
+        r412 = findViewById(R.id.r412);
+        r413 = findViewById(R.id.r413);
+        r414 = findViewById(R.id.r414);
+        r416 = findViewById(R.id.r416);
+        r417 = findViewById(R.id.r417);
+        r418 = findViewById(R.id.r418);
+        r419 = findViewById(R.id.r419);
         nodeList = new ArrayList<>();
         nodeList.add(r11);
         nodeList.add(r12);
@@ -97,33 +137,57 @@ public class MainActivity extends AppCompatActivity {
         nodeList.add(r217);
         nodeList.add(r218);
         nodeList.add(r219);
+        nodeList.add(r31);
+        nodeList.add(r32);
+        nodeList.add(r33);
+        nodeList.add(r34);
+        nodeList.add(r36);
+        nodeList.add(r37);
+        nodeList.add(r38);
+        nodeList.add(r39);
+        nodeList.add(r311);
+        nodeList.add(r312);
+        nodeList.add(r313);
+        nodeList.add(r314);
+        nodeList.add(r316);
+        nodeList.add(r317);
+        nodeList.add(r318);
+        nodeList.add(r319);
+        nodeList.add(r41);
+        nodeList.add(r42);
+        nodeList.add(r43);
+        nodeList.add(r44);
+        nodeList.add(r46);
+        nodeList.add(r47);
+        nodeList.add(r48);
+        nodeList.add(r49);
+        nodeList.add(r411);
+        nodeList.add(r412);
+        nodeList.add(r413);
+        nodeList.add(r414);
+        nodeList.add(r416);
+        nodeList.add(r417);
+        nodeList.add(r418);
+        nodeList.add(r419);
 
 
         convert.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
             @Override
             public void onClick(View view) {
                 int num = Integer.parseInt(input.getText().toString());
-                boolean done = false;
-                while (!done) {
-                    if (num >= 20) {
-                        num = tier2(num);
-                    }
-                    if (num >= 5) {
-                        nodeList.get(0 + counter).setVisibility(View.VISIBLE);
-                        for (int i = 3; i > 0; i--) {
-                            nodeList.get(i + counter).setVisibility(View.GONE);
-                        }
-                        counter = counter + 4;
-                        num = num - 5;
-                    } else if (num < 5) {
-                        for (int i = 0; i < num; i++) {
-                            nodeList.get(i + counter).setVisibility(View.VISIBLE);
-                        }
-                        done = true;
-                        counter = 0;
-                    }
+                if (num >= 8000) {
+                    num = tier4(num);
                 }
-
+                if (num >= 400) {
+                    num = tier3(num);
+                }
+                if (num >= 20) {
+                    num = tier2(num);
+                }
+                if (num >= 0) {
+                    tier1(num);
+                }
             }
         });
     }
@@ -150,21 +214,96 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+    private void tier1(int num) {
+        boolean done = false;
+        counter = 0;
+        while (!done) {
+            if (num >= 5) {
+                nodeList.get(counter).setVisibility(View.VISIBLE);
+                for (int i = 3; i > 0; i--) {
+                    nodeList.get(i + counter).setVisibility(View.GONE);
+                }
+                counter = counter + 4;
+                num = num - 5;
+            } else{
+                for (int i = 0; i < num; i++) {
+                    nodeList.get(i + counter).setVisibility(View.VISIBLE);
+                    Drawable d = getResources().getDrawable(R.drawable.mayan_circle);
+                    nodeList.get(i + counter).setBackground(d);
+                    nodeList.get(i + counter).setImageDrawable(d);
+
+                }
+                done = true;
+                counter = 0;
+            }
+        }
+    }
+
     private int tier2(int num) {
         boolean done = false;
         counter = 16;
         while (!done) {
             if (num >= 100) {
-                nodeList.get(0 + counter).setVisibility(View.VISIBLE);
+                nodeList.get(counter).setVisibility(View.VISIBLE);
                 for (int i = 3; i > 0; i--) {
                     nodeList.get(i + counter).setVisibility(View.GONE);
                 }
                 counter = counter + 4;
                 num = num - 100;
             } else if (num < 100) {
-                for (int i = 0; i < Math.floor(num / 20); i++) {
+                int repeat = (int)Math.floor(num / 20);
+                for (int i = 0; i < repeat; i++) {
                     nodeList.get(i + counter).setVisibility(View.VISIBLE);
                     num = num - 20;
+                }
+                done = true;
+                counter = 0;
+            }
+        }
+        return num;
+    }
+
+    private int tier3(int num) {
+        boolean done = false;
+        counter = 32;
+        while (!done) {
+            if (num >= 2000) {
+                nodeList.get(counter).setVisibility(View.VISIBLE);
+                for (int i = 3; i > 0; i--) {
+                    nodeList.get(i + counter).setVisibility(View.GONE);
+                }
+                counter = counter + 4;
+                num = num - 2000;
+            } else if (num < 2000) {
+                int repeat = (int)Math.floor(num / 400);
+                for (int i = 0; i < repeat; i++) {
+                    nodeList.get(i + counter).setVisibility(View.VISIBLE);
+                    num = num - 400;
+                }
+                done = true;
+                counter = 0;
+            }
+        }
+        return num;
+    }
+
+    private int tier4(int num) {
+        boolean done = false;
+        counter = 48;
+        while (!done) {
+            if (num >= 40000) {
+                nodeList.get(counter).setVisibility(View.VISIBLE);
+                for (int i = 3; i > 0; i--) {
+                    nodeList.get(i + counter).setVisibility(View.GONE);
+                }
+                counter = counter + 4;
+                num = num - 40000;
+            } else if (num < 40000) {
+                int repeat = (int)Math.floor(num / 8000);
+                for (int i = 0; i < repeat; i++) {
+                    nodeList.get(i + counter).setVisibility(View.VISIBLE);
+                    num = num - 8000;
                 }
                 done = true;
                 counter = 0;
